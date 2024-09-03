31e071a36778fa6c5ba60a9a850d3b590c74e47e
Allen Joseph <ajoseph@carestack.com>
2024-09-03 12:12:15 +0530

    removed unwated files

--- QOTD.Backend/obj/Debug/net8.0/QOTD.Backend.AssemblyInfo.cs
+++ QOTD.Backend/obj/Debug/net8.0/QOTD.Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QOTD.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faf8dba3150d3020068c37d4f5fce15aed3cb96d")]
 [assembly: System.Reflection.AssemblyProductAttribute("QOTD.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QOTD.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
